6e380c03c22f1119c15bea6a7a07b9aea911d97a
Suntossh <suntossh.numerouno@gmail.com>
2016-09-11 16:11:43 -0400

    Adding Comments Using JAutodoc plugin, Comment Template is not mature, it is a work in progress

--- src/com/suntossh/ws/soap/axis1/service/CalculatorService.java
+++ src/com/suntossh/ws/soap/axis1/service/CalculatorService.java
@@ -1,17 +1,44 @@
+/*
+ * 
+ */
 package com.suntossh.ws.soap.axis1.service;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CalculatorService.
+ */
 public class CalculatorService {
 
+	/**
+	 * Adds the.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the int
+	 */
 	public int add(int x, int y){
 		System.out.println("Add Operation invoked by the wsdl request.");
 		 return x+y;
 	}
 	
+	/**
+	 * Sub.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the int
+	 */
 	public int sub(int x, int y){
 		System.out.println("Sub Operation invoked by the wsdl request.");
 		return x-y;
 	}
 	
+	/**
+	 * Test service.
+	 *
+	 * @param YourName the your name
+	 * @return the string
+	 */
 	public String testService(String YourName){
 		return YourName+" You are Welcome, CalculatorService Service is Working \nit is Axis1 implementation of Jax-rpc provided by Apache Foundataion.";
 	}
